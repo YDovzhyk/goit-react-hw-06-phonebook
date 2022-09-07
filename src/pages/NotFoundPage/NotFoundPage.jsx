381862ee25581b39613fb53f4f8be70709ebb272
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2022-09-07 13:52:44 +0300

    add correction
    add correction

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,9 @@
+import s from "./NotFoundPage.module.css"
 
 const NotFoundPage = () => {
     return (
-        <div >
-            <img src={`https://img.freepik.com/free-vector/404-error-with-a-landscape-concept-illustration_114360-7888.jpg?w=2000`} alt="Page not found"  />
+        <div className={s.container}>
+            <img src={`https://img.freepik.com/free-vector/404-error-with-a-landscape-concept-illustration_114360-7888.jpg?w=2000`} alt="Page not found" className={s.image}/>
         </div>
     )
 }
