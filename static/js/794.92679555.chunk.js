31f90bae681a20094f7f4d80d84ca0e3d5df3c44
YDovzhyk <48929257+YDovzhyk@users.noreply.github.com>
2022-09-07 10:39:15 +0000

    Deploying to gh-pages from @ YDovzhyk/goit-react-hw-06-phonebook@4d2a4d7b2c8e5d79e63de956d258b7af822e629d 🚀

---
+++ static/js/794.92679555.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("img",{src:"https://img.freepik.com/free-vector/404-error-with-a-landscape-concept-illustration_114360-7888.jpg?w=2000",alt:"Page not found"})})}}}]);
+//# sourceMappingURL=794.92679555.chunk.js.map
